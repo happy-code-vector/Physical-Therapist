f670ff0055f251b9790bd668f9e96fa511246b7a
happy-code-vector <kevinallen5877066@gmail.com>
2026-08-03 16:11:21 +0300

    fix: SEO phase 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://faastpt.example',
+  site: 'https://faastpt.com',
   output: 'static',
   integrations: [sitemap()],
 });
